889c8b2f51706c9d4a76af82761a5cf1364280ad
Wesleyy06 <wesleydesousa34@gmail.com>
2023-10-29 16:24:21 -0300

    Pequena refatoração no codigo e correção no laço de repetição while

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,93 +1,87 @@
-﻿
-using System.Runtime.Serialization;
-using System.Threading.Channels;
-
-partial class Program
+﻿partial class Program
 {
     static void Main(string[] args)
     {
-        int x = 0;
-        int y = 0;
+        double x, y;  
 
         double resultado = 0;
 
-        FormatarTitulo("***   Calculadora console   ***");
-        Console.WriteLine("Digite [1] para somar, [2] para subtrair, [3] para multiplicar e [4] para dividir e [5] para encerrar a aplicação");
+        int operacaoEscolhida = 0;
 
-        int opcaoEscolhida = 0;
         try
         {
-            while (opcaoEscolhida != 5)
+            
+            while (operacaoEscolhida != 5)
             {
+                FormatarTitulo("***   Calculadora console   ***");
+                Console.WriteLine("Digite [1] para somar, [2] para subtrair, [3] para multiplicar e [4] para dividir e [5] para encerrar a aplicação");
+
                 try
                 {
-                    opcaoEscolhida = int.Parse(Console.ReadLine()!);
+                    operacaoEscolhida = int.Parse(Console.ReadLine()!);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
 
-                if (opcaoEscolhida == 1)
+                if (operacaoEscolhida == 1)
                 {
                     Console.Clear();
                     Console.Write("1º valor: ");
-                    x = int.Parse(Console.ReadLine()!);
+                    x = double.Parse(Console.ReadLine()!);
                     Console.Write("2º valor: ");
-                    y = int.Parse(Console.ReadLine()!);
+                    y = double.Parse(Console.ReadLine()!);
                     resultado = soma(x, y);
                     ExibirResultado(resultado);
-                    return;
                 }
 
-                if (opcaoEscolhida == 2)
+                if (operacaoEscolhida == 2)
                 {
                     Console.Write("1º valor: ");
-                    x = int.Parse(Console.ReadLine());
+                    x = double.Parse(Console.ReadLine());
                     Console.Write("2º valor: ");
-                    y = int.Parse(Console.ReadLine());
+                    y = double.Parse(Console.ReadLine());
                     resultado = subtracao(x, y);
                     ExibirResultado(resultado);
-                    return;
                 }
 
-                if (opcaoEscolhida == 3)
+                if (operacaoEscolhida == 3)
                 {
                     Console.Write("1º valor: ");
-                    x = int.Parse(Console.ReadLine());
+                    x = double.Parse(Console.ReadLine());
                     Console.Write("2º valor: ");
-                    y = int.Parse(Console.ReadLine());
+                    y = double.Parse(Console.ReadLine());
                     resultado = multiplicacao(x, y);
                     ExibirResultado(resultado);
-                    return;
                 }
 
-                if (opcaoEscolhida == 4)
+                if (operacaoEscolhida == 4)
                 {
                     Console.Write("1º valor: ");
-                    x = int.Parse(Console.ReadLine());
+                    x = double.Parse(Console.ReadLine());
                     Console.Write("2º valor: ");
-                    y = int.Parse(Console.ReadLine());
+                    y = double.Parse(Console.ReadLine());
                     resultado = multiplicacao(x, y);
                     ExibirResultado(resultado);
-                    return;
                 }
+                Console.ReadKey();
+                Console.Clear();
             }
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
-        
     }
     static void ExibirResultado(double resultado)
     {
         Console.Clear();
         Console.WriteLine($"Resultado: [{resultado}]");
-        Console.ReadKey();
+        return;
     }
 
-   static void FormatarTitulo(string titulo)
+    static void FormatarTitulo(string titulo)
     {
         int tamanhoDoTitulo = titulo.Length;
         string escrever = string.Empty.PadLeft(tamanhoDoTitulo, '*');
